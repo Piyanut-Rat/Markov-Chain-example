deab301b454550ec2818647df46c381d9171194a
Piyanut Ratphibun <77672038+Piyanut-Rat@users.noreply.github.com>
2021-12-22 18:28:28 +0700

    Create Markov-example-concept.py

---
+++ Markov-example-concept.py
@@ -0,0 +1,18 @@
+#import library
+
+
+#input parameters
+
+
+#sequence generator
+
+
+#est trans prob: Makov learning base on counting method
+
+                        
+#est init prob
+
+
+#predict function
+
+
